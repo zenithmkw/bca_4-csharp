0ef3606eccd34b21332afcee9e5b24f6e43b24cd
zenithmkw <zenith.makwana126980@marwadiuniversity.ac.in>
2026-03-23 10:25:09 +0530

    prog20 of unit 4

---
+++ prog20/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle r1 = new rectangle();
+            r1.calculate();
+            r1.display();
+
+            rectangle r2 = new rectangle(30, 40);
+            r2.calculate();
+            r2.display();
+        }
+    }
+}
---
+++ prog20/rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consstructor
+{
+    internal class rectangle
+    {
+        int l, w, area;
+        public rectangle()
+        {
+            l = 10;
+            w = 20;
+        }
+        public rectangle(int l1, int w1)
+        {
+            l = l1;
+            w = w1;
+        }
+        public void calculate()
+        {
+            area = l * w;
+        }
+        public void display()
+        {
+            Console.WriteLine("area of rectangle is: " + area);
+        }
+
+    }
+}
